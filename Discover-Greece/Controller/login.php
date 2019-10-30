d9c81a41698c2aa96d8211b83347ea80a358fa97
Nick Potiriadis <32678324+NickyPot@users.noreply.github.com>
2019-10-30 03:33:37 +0000

    added 1st version of recaptcha to login

--- Discover-Greece/Controller/login.php
+++ Discover-Greece/Controller/login.php
@@ -3,30 +3,45 @@
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
-
-
   session_start();
 
   include("/home/UAD/1704807/public_html/Model/api-user.php");
 
   $data -> userName = $_POST["username"];
   $data -> userPass = $_POST["password"];
-  $jsonData = json_encode($data);
-  $res = getUser($jsonData);
-  echo $res;
+  $recaptchaKey = "6LcOKMAUAAAAACTmKSg0GpUvPTNhOEIItVlJU4o7";
+  $userIp = $_SERVER['REMOTE_ADDR'];
+  $recaptchaResp = $_POST['g-recaptcha-response'];
+  $url = "https://www.google.com/recaptcha/api/siteverify?secret=$recaptchaKey&response=$recaptchaResp&remoteip=$userIp";
+  $isBot = file_get_contents($url);
+  $isBot = json_decode($isBot);
+
+  if ($isBot ->success) {
+    echo "success";
+
+    $jsonData = json_encode($data);
+    $res = getUser($jsonData);
+    echo $res;
 
 
-  if ($res > 0){
+    if ($res > 0){
 
-    $_SESSION["loggedIn"] = true;
+      $_SESSION["loggedIn"] = true;
+    }
+
+  else {
+        $_SESSION["loggedIn"] = false;
   }
 
-else {
-      $_SESSION["loggedIn"] = false;
-}
+
+  header ("Location: /~1704807/index.php");
 
 
-header ("Location: /~1704807/index.php");
+  }
+
+  else {
+    echo "false";
+  }
 
 
 ?>
