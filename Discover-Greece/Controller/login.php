a8bfba0d460d0f85bf0fa6d61f261c386eac1e3c
Nick Potiriadis <32678324+NickyPot@users.noreply.github.com>
2019-10-31 02:11:07 +0000

    verifies if the user has completed the email, pass and captcha

--- Discover-Greece/Controller/login.php
+++ Discover-Greece/Controller/login.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Bootstrap 4 Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/~1704807/View/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
 <?php
 
 ini_set("display_errors", 1);
@@ -7,41 +22,62 @@ error_reporting(E_ALL);
 
   include("/home/UAD/1704807/public_html/Model/api-user.php");
 
-  $data -> userName = $_POST["username"];
-  $data -> userPass = $_POST["password"];
-  $recaptchaKey = "6LcOKMAUAAAAACTmKSg0GpUvPTNhOEIItVlJU4o7";
-  $userIp = $_SERVER['REMOTE_ADDR'];
-  $recaptchaResp = $_POST['g-recaptcha-response'];
-  $url = "https://www.google.com/recaptcha/api/siteverify?secret=$recaptchaKey&response=$recaptchaResp&remoteip=$userIp";
-  $isBot = file_get_contents($url);
-  $isBot = json_decode($isBot);
-
-  if ($isBot ->success) {
-    echo "success";
-
-    $jsonData = json_encode($data);
-    $res = getUser($jsonData);
-    echo $res;
+//if the email, password or reCaptcha have NOT been filled out then do this
+if (empty($_POST["username"]) or empty($_POST["password"]) or empty($_POST["g-recaptcha-response"]))
+{
+  include ("/home/UAD/1704807/public_html/View/login-form.php");
+  echo "<p id='formAlert'>Please make sure you have filled out your email, password and have completed the captcha!</p>";
 
 
-    if ($res > 0){
+}
 
-      $_SESSION["loggedIn"] = true;
+//if not everything was filled out
+else
+{
+
+    $data -> userName = $_POST["username"];
+    $data -> userPass = $_POST["password"];
+    $recaptchaKey = "6LcOKMAUAAAAACTmKSg0GpUvPTNhOEIItVlJU4o7";
+    $userIp = $_SERVER['REMOTE_ADDR'];
+    $recaptchaResp = $_POST['g-recaptcha-response'];
+    $url = "https://www.google.com/recaptcha/api/siteverify?secret=$recaptchaKey&response=$recaptchaResp&remoteip=$userIp";
+    $isBot = file_get_contents($url);
+    $isBot = json_decode($isBot);
+
+  //if the reCaptcha was successfull
+    if ($isBot ->success) {
+      echo "success";
+
+      $jsonData = json_encode($data);
+      $res = getUser($jsonData);
+      echo $res;
+
+    //if there is at least one user
+      if ($res > 0){
+
+        $_SESSION["loggedIn"] = true;
+      }
+
+    //if there is no users
+    else {
+          $_SESSION["loggedIn"] = false;
     }
 
-  else {
-        $_SESSION["loggedIn"] = false;
-  }
+
+    header ("Location: /~1704807/index.php");
 
 
-  header ("Location: /~1704807/index.php");
+    }
+
+    //if you are a bot
+    else {
+      echo "false";
+    }
 
 
-  }
-
-  else {
-    echo "false";
-  }
-
-
+}
 ?>
+
+</body>
+
+</html>
--- Discover-Greece/View/login-form.php
+++ Discover-Greece/View/login-form.php
@@ -7,8 +7,9 @@
 		<form action="../Controller/login.php" method="post">
 			<li>User Name<input type="text" name="username" /></li>
 			<li>Employee Name<input type="password" name="password" /></li>
-			<div class="g-recaptcha" data-sitekey="6LcOKMAUAAAAAOPE6jQqGjjRQOzbH3jMrxA9CGAT"></div>
-			<li><input type="submit"></li>
+			<li id="isChecked"></li>
+			<div class="g-recaptcha" id="reCaptcha" data-sitekey="6LcOKMAUAAAAAOPE6jQqGjjRQOzbH3jMrxA9CGAT"></div>
+			<li><input type="submit" onclick=""></li>
 		</form>
 
 </body>
