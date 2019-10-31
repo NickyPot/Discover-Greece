7fc278bdcb11b9f453db867222df9a22013cb68b
Nick Potiriadis <32678324+NickyPot@users.noreply.github.com>
2019-10-31 22:48:34 +0000

    added js validation of password

--- Discover-Greece/Controller/scripts.js
+++ Discover-Greece/Controller/scripts.js
@@ -16,5 +16,81 @@ function loginValidation()
     return false;
   }
 
+}
+
+
+var passwordField = document.getElementById('password');
+var small = document.getElementById('small');
+var capital = document.getElementById('capital');
+var number = document.getElementById('number');
+var specialChar = document.getElementById('specialChar');
+var length = document.getElementById('length');
+
+//shows the password guidlines if the user is in the password field
+passwordField.onfocus = function(){
+
+  document.getElementById('passwordVer').style.display = "block";
+
+}
+
+//hides the password guidlines if the user is NOT in the password field
+passwordField.onblur = function(){
+
+  document.getElementById('passwordVer').style.display = "none";
+
+}
+
+
+// When the user starts to type something inside the password field
+passwordField.onkeyup = function() {
+
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(passwordField.value.match(lowerCaseLetters)) {
+    small.classList.remove("invalid");
+    small.classList.add("valid");
+  } else {
+    small.classList.remove("valid");
+    small.classList.add("invalid");
+  }
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(passwordField.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(passwordField.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(passwordField.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+
+  // Validate special char
+  var numbers = /[!£$%^&*?#@]/g;
+  if(passwordField.value.match(numbers)) {
+    specialChar.classList.remove("invalid");
+    specialChar.classList.add("valid");
+  } else {
+    specialChar.classList.remove("valid");
+    specialChar.classList.add("invalid");
+  }
 
 }
--- Discover-Greece/View/login-form.php
+++ Discover-Greece/View/login-form.php
@@ -1,18 +1,65 @@
 <html>
 <head>
+	<title>Bootstrap 4 Example</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/~1704807/View/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 	 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 <body>
 
-		<form action="../Controller/login.php" method="post" onsubmit = "return loginValidation()">
+
+	  <div>
+
+	    <!-- source: https://wallpapermemory.com/uploads/705/santorini-wallpaper-1080p-496872.jpg-->
+	    <img class="img-responsive center-block fit-image" src="/~1704807/imgs/headerImage2.jpg" alt="header image">
+	  </div>
+
+	  <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+
+	    <ul class="navbar-nav">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="#">Active</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">Link</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">Link</a>
+	      </li>
+
+
+
+
+
+	    </ul>
+
+	  </nav>
+
+<div class="card">
+
+
+		<form name ="loginForm" action="../Controller/login.php" method="post" onsubmit = "return loginValidation()">
 			<li>User Name<input type="text" name="username" id="username" /></li>
-			<li>Employee Name<input type="password" name="password" id="password" /></li>
+			<li>Password<input type="password" name="password" id="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!£$%^&*?#@]).{8,}"/></li>
 			<li id="isChecked"></li>
 			<div class="g-recaptcha" id="reCaptcha" data-sitekey="6LcOKMAUAAAAAOPE6jQqGjjRQOzbH3jMrxA9CGAT"></div>
+			<div id="passwordVer">
+				<li id="small" class="invalid">Please include lower case letters in your password!</li>
+				<li id="capital" class="invalid">Please include a capital letter in your password!</li>
+				<li id="number" class="invalid">Please include a number in your password!</li>
+				<li id="specialChar" class="invalid">Please include a special character (!, £, $, %, ^, &, *, ?, #, @) in your password!</li>
+				<li id="length" class="invalid">Your password needs to be at least 8 characters long!</li>
+			</div>
+
 			<li><input type="submit"></li>
 		</form>
 
-
+</div>
 
 <script src="../Controller/scripts.js"></script>
 </body>
